599ac35b4f253fd2c11596fda1108878b97aea32
David McGowan <david.mcgowan@screenmedia.co.uk>
2018-03-23 16:12:57 +0000

    initial commit

---
+++ Scripts/app/controllers/mainController.js
@@ -0,0 +1,18 @@
+﻿angular.module('sonosApp', [])
+    .controller('homeController', function ($scope, $http) {
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:5005/studio%20403/state'
+        }).then(function successCallback (response) {
+            $scope.CurrentTrack = response.data.currentTrack;
+            $scope.NextTrack = response.data.nextTrack;
+            $scope.PlaybackState = response.data.playbackState;
+            $scope.Mute = response.data.mute;
+            $scope.Volume = response.data.volume;
+            }, function errorCallback(response) {
+            console.log(response);
+            $scope.Offline = true;
+        });
+
+    });
