9e796e61e74fcc68889215eb042e8cd8b13816dd
David McGowan <david.mcgowan@screenmedia.co.uk>
2018-03-27 09:52:37 +0100

    added simple poll on get state call

--- SonosApp/Scripts/app/controllers/MainController.js
+++ SonosApp/Scripts/app/controllers/MainController.js
@@ -1,18 +1,23 @@
 ﻿angular.module('sonosApp', [])
     .controller('homeController', function ($scope, $http) {
 
-        $http({
-            method: 'GET',
-            url: 'http://localhost:5005/studio%20403/state'
-        }).then(function successCallback (response) {
-            $scope.CurrentTrack = response.data.currentTrack;
-            $scope.NextTrack = response.data.nextTrack;
-            $scope.PlaybackState = response.data.playbackState;
-            $scope.Mute = response.data.mute;
-            $scope.Volume = response.data.volume;
-        }, function errorCallback(response) {
-            console.log(response);
-            $scope.Offline = true;
-        });
+        function doPoll(){
+            $http({
+                method: 'GET',
+                url: 'http://localhost:5005/studio%20403/state'
+            }).then(function successCallback (response) {
+                $scope.CurrentTrack = response.data.currentTrack;
+                $scope.NextTrack = response.data.nextTrack;
+                $scope.PlaybackState = response.data.playbackState;
+                $scope.Mute = response.data.mute;
+                $scope.Volume = response.data.volume;
+                setTimeout(doPoll,5000);
+            }, function errorCallback(response) {
+                console.log(response);
+                $scope.Offline = true;
+            });
+        }
+
+        doPoll();
 
     });
