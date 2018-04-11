367c21350b4e00f674642e9d0ed757ca24d505d5
David McGowan <david.mcgowan@screenmedia.co.uk>
2018-04-11 16:54:28 +0100

    effin about

--- SonosApp/Scripts/app/controllers/MainController.js
+++ SonosApp/Scripts/app/controllers/MainController.js
@@ -33,20 +33,53 @@
                 $scope.Offline = true;
             });
 
-            //403 Queue
-            $http({
-                method: 'GET',
-                url: 'http://localhost:5005/studio%20403/queue'
-            }).then(function successCallback(response) {
-                $scope.Queue = response.data;
-            }, function errorCallback(response) {
-                console.log(response);
-                $scope.Offline = true;
-            });
+            ////403 Queue
+            //$http({
+            //    method: 'GET',
+            //    url: 'http://localhost:5005/studio%20403/queue/10'
+            //}).then(function successCallback(response) {
+            //    $scope.Queue = response.data;
+            //}, function errorCallback(response) {
+            //    console.log(response);
+            //    $scope.Offline = true;
+            //});
 
             setTimeout(doPoll, 5000);
 
+            //var now = new Date();
+            //var hour = now.getHours();
+            //var minutes = now.getMinutes();
+
+            //if (hour == 16 && minutes == 48) {
+
+            //    if ($scope.CurrentTrack403.artist == "Oasis") {
+            //        console.log("already playing " + $scope.CurrentTrack403.artist);
+            //    }
+
+            //};
+
         }
+
+        //function playOasis() {
+            //console.log("now playing oasis");
+
+            //supersonic - spotify:track:1ZAWaJppxtA3zPh1R2W2wS
+            //spotify:track:726Bm6HoMMOwrJlxqbfO45
+            //spotify:track:0Uk0BWIMooZTyACeU16T9t
+            //spotify:track:4ETiD213qgNHNYKfZRLlsH
+
+            //var id = '4ETiD213qgNHNYKfZRLlsH';
+
+            //$http({
+            //    method: 'GET',
+            //    url: 'http://localhost:5005/studio%20403/spotify/now/spotify:track:' + id
+            //}).then(function successCallback(response) {
+            //    console.log(response);
+            //}, function errorCallback(response) {
+            //    console.log(response);
+            //    $scope.Offline = true;
+            //});
+        //}
 
         //$scope.showQueue = function() {
 
